d85d0fb146102288cecd2edac8247695cf9a7d07
JustGimli <aleksnermer@gmail.com>
2022-11-24 03:14:21 +0300

    test sfml

--- Makefile
+++ Makefile
@@ -3,8 +3,7 @@ CFLAGS = -lsfml-graphics -lsfml-window -lsfml-system
 
 all:
 	$(CC) -c main/main.cpp -I SFML-2.5.1/include
-	$(CC) main.o -o sfml-app -L SFML-2.5.1/lib 	$(CFLAGS)
-	app
-
+	$(CC) main.o -o app -L SFML-2.5.1/lib 	$(CFLAGS)
+	export LD_LIBRARY_PATH=SFML-2.5.1/lib  && ./app
 clean:
-	rm -rf main.o
+	rm -rf ../main.o	
--- main/main.cpp
+++ main/main.cpp
@@ -5,8 +5,47 @@ int main()
     sf::Clock clock;
     while(true)
     {
-        sf::Time timeelapsed = clock.getElapsedTime();
-        std::cout << timeelapsed.asSeconds();
-        std::cin.get();
+        // sf::FileInputStream stream;
+        // stream.open("./main/image.png");
+        // sf::Texture texture;
+        // texture.loadFromStream(stream);
+        // sf::Time timeelapsed = clock.getElapsedTime();
+        // std::cout << timeelapsed.asSeconds();
+        // int s = timeelapsed.asSeconds();
+        // if( s > 5)
+        //     std::cout <<"5 sec";
+        // std::cin.get();
+        sf::Window window(sf::VideoMode(800,600), "Timer",sf::Style::Default);
+        while(window.isOpen())
+        {
+            sf::Event event;
+
+            while(window.pollEvent(event))
+            {
+                sf::Vector2i gloabalpos = sf::Mouse::getPosition();
+                sf::Vector2i localpos = sf::Mouse::getPosition(window);
+                std::cout << "global:  " << gloabalpos << std::endl;
+                std::cout << "local " << localpos << std::endl;  
+                // if(event.type == sf::Event::Closed)
+                //     window.close();
+
+                // else if (event.type == sf::Event::MouseEntered)// switch 
+                // {
+                //     sf::Time timeelapsed = clock.getElapsedTime();
+                //     std::cout << "time" << timeelapsed.asSeconds(); 
+                //     window.setTitle("asd");
+                //     window.setPosition(sf::Vector2i(10,50));
+                // }
+                    
+                // else if(event.type == sf::Event::MouseLeft)
+                
+                //     window.setTitle("timer");
+                
+                // bool focus = window.hasFocus();
+                // if(focus)
+                //     window.setTitle("ggggg");
+                    
+            }
+        }
     }
 }
