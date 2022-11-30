246699c1a913ca5fb442361503fb89c8e0bda4e4
JustGimli <aleksnermer@gmail.com>
2022-12-01 02:54:44 +0300

    add timer

--- Makefile
+++ Makefile
@@ -5,5 +5,7 @@ all:
 	$(CC) -c main/main.cpp -I SFML-2.5.1/include
 	$(CC) main.o -o app -L SFML-2.5.1/lib 	$(CFLAGS)
 	export LD_LIBRARY_PATH=SFML-2.5.1/lib  && ./app
+	rm  ./main.o	
 clean:
-	rm -rf ../main.o	
+	rm  ./main.o	
+
--- main/main.cpp
+++ main/main.cpp
@@ -1,51 +1,65 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <sstream>
+#include <string>
+
+
+typedef std::string  str;
+
+str timeToStr(const float & seconds);
+
+
 int main()
 {
-    sf::Clock clock;
-    while(true)
+    sf::Text text;
+        //fronts
+    sf::Font font;
+    
+    if(!font.loadFromFile("./font/OpenSans-Bold.ttf"))
     {
-        // sf::FileInputStream stream;
-        // stream.open("./main/image.png");
-        // sf::Texture texture;
-        // texture.loadFromStream(stream);
-        // sf::Time timeelapsed = clock.getElapsedTime();
-        // std::cout << timeelapsed.asSeconds();
-        // int s = timeelapsed.asSeconds();
-        // if( s > 5)
-        //     std::cout <<"5 sec";
-        // std::cin.get();
-        sf::Window window(sf::VideoMode(800,600), "Timer",sf::Style::Default);
-        while(window.isOpen())
+        //error
+    }    
+    text.setFont(font);
+    sf::Clock clock;
+    sf::RenderWindow window(sf::VideoMode(800,600), "Timer",sf::Style::Close); //render window
+    window.setFramerateLimit(60);
+
+    while(window.isOpen())
+    {
+        sf::Event event;
+
+        while(window.pollEvent(event))
         {
-            sf::Event event;
 
-            while(window.pollEvent(event))
+
+            if (event.type == sf::Event::Closed)
             {
-                sf::Vector2i gloabalpos = sf::Mouse::getPosition();
-                sf::Vector2i localpos = sf::Mouse::getPosition(window);
-                std::cout << "global:  " << gloabalpos << std::endl;
-                std::cout << "local " << localpos << std::endl;  
-                // if(event.type == sf::Event::Closed)
-                //     window.close();
-
-                // else if (event.type == sf::Event::MouseEntered)// switch 
-                // {
-                //     sf::Time timeelapsed = clock.getElapsedTime();
-                //     std::cout << "time" << timeelapsed.asSeconds(); 
-                //     window.setTitle("asd");
-                //     window.setPosition(sf::Vector2i(10,50));
-                // }
-                    
-                // else if(event.type == sf::Event::MouseLeft)
-                
-                //     window.setTitle("timer");
-                
-                // bool focus = window.hasFocus();
-                // if(focus)
-                //     window.setTitle("ggggg");
-                    
+                window.close();
             }
+            else if(event.type == sf::Event::LostFocus) {}
+
+
+      
         }
+
+       
+        sf::Time elapsedTime = clock.getElapsedTime();
+        
+        text.setString(timeToStr(elapsedTime.asSeconds()));
+        text.setCharacterSize(24);
+        window.clear();
+        window.draw(text);
+        window.display();
+
+   
     }
+
 }
+
+
+str timeToStr(const float & seconds)
+{
+    int seconds_int = seconds;
+    return std::to_string(seconds_int) ;
+}
+
